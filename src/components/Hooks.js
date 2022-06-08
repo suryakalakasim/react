b6e46dde3f596a72330a7caaa42b5f830207167a
suryakalakasim <chitarkalaksm@gmail.com>
2022-06-08 19:53:29 +0530

    modify

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -6,7 +6,7 @@ function UseStatehook(){
     const[value,setvalue]=useState([{color:"green",brand:"oppo"},{color:"yelllow",brand:"apple"},{color:"blue",brand:"vivo"}])
 console.log("value>>",value)
 const object= value.map(({color,brand})=>{
-     return //<p>the mobile color {color} and its brand {brand}</p>
+     return 
 })
     return<div>
         <h2>my name is {state.name} my age {state.age} my marks are{state.marks}</h2>
